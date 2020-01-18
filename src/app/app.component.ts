5e776197ca28c41fda7507be2537c4412e9403c1
nsaniti <jaylees@live.com>
2020-01-18 03:44:38 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import  Component  from '@angular/core';
 
-@Component({
+@Component(
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
-})
+)
 import class App Component  
   name = 'Jay';
 
